4b5715da0103dfccb10144bf75750a60bb3f2bd2
Link-kkkkk <13788485679@sina.cn>
2018-08-14 13:57:32 +0800

    md

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,17 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
+    "no-trailing-spaces": ["error", { "skipBlankLines": true }],
+    // 句末不需要分号（当然for等里面必须的还是要的）
+    "semi": ["error", "never"],
+    "semi-spacing": ["error", { "before": false, "after": true }],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="home"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import * as THREE from 'three'
+
+function installThreeJs () {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+  
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  document.getElementById('home').appendChild( renderer.domElement )
+}
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  mounted () {
+    installThreeJs()
+  },
+  methods: {
+
   }
 }
 </script>
